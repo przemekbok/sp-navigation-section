d78bf9081875ad0e69d9c29c80b6dc692f0479c7
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-05-27 13:04:23 +0200

    Update props interface for navigation webpart

--- src/webparts/spNavigationSection/components/ISpNavigationSectionProps.ts
+++ src/webparts/spNavigationSection/components/ISpNavigationSectionProps.ts
@@ -4,4 +4,17 @@ export interface ISpNavigationSectionProps {
   environmentMessage: string;
   hasTeamsContext: boolean;
   userDisplayName: string;
+  selectedListId: string;
+  navigationItems: INavigationItem[];
+  siteUrl: string;
+}
+
+export interface INavigationItem {
+  displayText: string;
+  link: string;
+}
+
+export interface IListInfo {
+  id: string;
+  title: string;
 }
