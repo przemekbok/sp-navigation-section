08054aee0a05ca7a494dbb9a2c3e4e77e7582adf
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-05-27 13:05:30 +0200

    Update webpart with SharePoint list integration and property pane

--- src/webparts/spNavigationSection/SpNavigationSectionWebPart.ts
+++ src/webparts/spNavigationSection/SpNavigationSectionWebPart.ts
@@ -3,23 +3,30 @@ import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
 import {
   type IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField,
+  PropertyPaneDropdown,
+  PropertyPaneLink,
+  IPropertyPaneDropdownOption
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 
 import * as strings from 'SpNavigationSectionWebPartStrings';
 import SpNavigationSection from './components/SpNavigationSection';
-import { ISpNavigationSectionProps } from './components/ISpNavigationSectionProps';
+import { ISpNavigationSectionProps, INavigationItem, IListInfo } from './components/ISpNavigationSectionProps';
 
 export interface ISpNavigationSectionWebPartProps {
   description: string;
+  selectedListId: string;
 }
 
 export default class SpNavigationSectionWebPart extends BaseClientSideWebPart<ISpNavigationSectionWebPartProps> {
 
   private _isDarkTheme: boolean = false;
   private _environmentMessage: string = '';
+  private _lists: IListInfo[] = [];
+  private _navigationItems: INavigationItem[] = [];
 
   public render(): void {
     const element: React.ReactElement<ISpNavigationSectionProps> = React.createElement(
@@ -29,7 +36,10 @@ export default class SpNavigationSectionWebPart extends BaseClientSideWebPart<IS
         isDarkTheme: this._isDarkTheme,
         environmentMessage: this._environmentMessage,
         hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        userDisplayName: this.context.pageContext.user.displayName,
+        selectedListId: this.properties.selectedListId,
+        navigationItems: this._navigationItems,
+        siteUrl: this.context.pageContext.web.absoluteUrl
       }
     );
 
@@ -39,10 +49,74 @@ export default class SpNavigationSectionWebPart extends BaseClientSideWebPart<IS
   protected onInit(): Promise<void> {
     return this._getEnvironmentMessage().then(message => {
       this._environmentMessage = message;
+      return this._loadLists();
+    }).then(() => {
+      if (this.properties.selectedListId) {
+        return this._loadNavigationItems();
+      }
+      return Promise.resolve();
     });
   }
 
+  protected onPropertyPaneFieldChanged(propertyPath: string, oldValue: any, newValue: any): void {
+    if (propertyPath === 'selectedListId' && newValue !== oldValue) {
+      this.properties.selectedListId = newValue;
+      this._loadNavigationItems().then(() => {
+        this.render();
+      });
+    }
+  }
 
+  private _loadLists(): Promise<void> {
+    const url = `${this.context.pageContext.web.absoluteUrl}/_api/web/lists?$filter=Hidden eq false&$select=Id,Title&$orderby=Title`;
+    
+    return this.context.spHttpClient.get(url, SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`Failed to load lists: ${response.statusText}`);
+        }
+      })
+      .then((data: any) => {
+        this._lists = data.value.map((list: any) => ({
+          id: list.Id,
+          title: list.Title
+        }));
+      })
+      .catch((error) => {
+        console.error('Error loading lists:', error);
+        this._lists = [];
+      });
+  }
+
+  private _loadNavigationItems(): Promise<void> {
+    if (!this.properties.selectedListId) {
+      this._navigationItems = [];
+      return Promise.resolve();
+    }
+
+    const url = `${this.context.pageContext.web.absoluteUrl}/_api/web/lists('${this.properties.selectedListId}')/items?$select=Title,Display_x0020_Text,Link&$orderby=ID`;
+    
+    return this.context.spHttpClient.get(url, SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`Failed to load navigation items: ${response.statusText}`);
+        }
+      })
+      .then((data: any) => {
+        this._navigationItems = data.value.map((item: any) => ({
+          displayText: item.Display_x0020_Text || item.Title,
+          link: item.Link?.Url || item.Link || '#'
+        }));
+      })
+      .catch((error) => {
+        console.error('Error loading navigation items:', error);
+        this._navigationItems = [];
+      });
+  }
 
   private _getEnvironmentMessage(): Promise<string> {
     if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
@@ -98,6 +172,15 @@ export default class SpNavigationSectionWebPart extends BaseClientSideWebPart<IS
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    const listOptions: IPropertyPaneDropdownOption[] = this._lists.map(list => ({
+      key: list.id,
+      text: list.title
+    }));
+
+    const selectedList = this._lists.find(list => list.id === this.properties.selectedListId);
+    const newListUrl = `${this.context.pageContext.web.absoluteUrl}/_layouts/15/new.aspx?List={lists}`;
+    const listViewUrl = selectedList ? `${this.context.pageContext.web.absoluteUrl}/Lists/${selectedList.title}` : '#';
+
     return {
       pages: [
         {
@@ -106,10 +189,26 @@ export default class SpNavigationSectionWebPart extends BaseClientSideWebPart<IS
           },
           groups: [
             {
-              groupName: strings.BasicGroupName,
+              groupName: 'Navigation Settings',
               groupFields: [
                 PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
+                  label: 'Header Text'
+                }),
+                PropertyPaneDropdown('selectedListId', {
+                  label: 'Select Navigation List',
+                  options: listOptions,
+                  selectedKey: this.properties.selectedListId
+                }),
+                PropertyPaneLink('', {
+                  href: newListUrl,
+                  text: 'Create New List',
+                  target: '_blank'
+                }),
+                PropertyPaneLink('', {
+                  href: listViewUrl,
+                  text: 'View Selected List',
+                  target: '_blank',
+                  disabled: !this.properties.selectedListId
                 })
               ]
             }
