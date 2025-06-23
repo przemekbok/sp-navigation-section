45d3e0bd75c373b3eebf7ff9a98e885e6a4084a1
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-06-23 02:14:36 +0200

    feat: add section support to interfaces

--- src/webparts/spNavigationSection/components/ISpNavigationSectionProps.ts
+++ src/webparts/spNavigationSection/components/ISpNavigationSectionProps.ts
@@ -5,15 +5,21 @@ export interface ISpNavigationSectionProps {
   hasTeamsContext: boolean;
   userDisplayName: string;
   selectedListId: string;
-  navigationItems: INavigationItem[];
+  navigationSections: INavigationSection[];
   siteUrl: string;
   isLoading: boolean;
   errorMessage: string;
 }
 
+export interface INavigationSection {
+  section: string;
+  items: INavigationItem[];
+}
+
 export interface INavigationItem {
   displayText: string;
   link: string;
+  section: string;
 }
 
 export interface IListInfo {
